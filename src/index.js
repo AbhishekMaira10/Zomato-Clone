21a2a1d8fbc86787dcbf24d8677e0bdff0765889
AbhishekMaira10 <abhishekmaira1999@gmail.com>
2021-03-01 23:27:09 +0530

    useState concept

--- src/index.js
+++ src/index.js
@@ -1,25 +1,54 @@
-import React from 'react';
+import React , {useState} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function Salutation ({fname,lname}) {
-  return <h1>Hello {fname} {lname}</h1>;
-}
+// function Salutation ({fname,lname}) {
+//   return <h3>Hello {fname} {lname}</h3>;
+// }
 
 function SaluteAll() {
-  let info = [
-    ['Abhishek', 'Maira'],
-    ['Anuj', 'Garg'],
-    ['Katrina', 'Kaif'],
-  ]
+
+  let [counter, setCounter] = useState(0);
+
+  let update = () => {
+    setCounter(counter + 1);
+  }
+
+//   let info = [
+//     ['Abhishek', 'Maira'],
+//     ['Anuj', 'Garg'],
+//     ['Katrina', 'Kaif'],
+//   ]
+  
+
+// let infoSecond = [];
+
+// let shift = function() {
+//   if(info.length > 0)
+//   {
+//     let item = info.pop();
+//     infoSecond.push(item);
+//   }
+// }
 
   return (
-  <div>
-    {info.map((item) => {
-      return < Salutation fname={item[0]} lname={item[1]}/>;
-    })}
-  </div>
-  );
+    <div>
+      <button onClick={update}>Click me</button>
+      <h1>{counter}</h1>
+      {/* <h1>List One</h1>
+      <div>
+        {info.map((item) => {
+          return <Salutation fname={item[0]} lname={item[1]} />
+        })}
+      </div>
+      <h1>List Two</h1>
+      <div>
+        {infoSecond.map((item) => {
+          return <Salutation fname={item[0]} lname={item[1]} />
+        })}
+      </div> */}
+    </div>
+  )
 }
 
 ReactDOM.render(
